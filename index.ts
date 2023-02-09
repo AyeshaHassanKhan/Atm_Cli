80023d6fdb8ccec1a0044535a0b26c0773c39b93
AyeshaHassanKhan <115189271+AyeshaHassanKhan@users.noreply.github.com>
2023-02-09 19:57:46 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,55 @@
+import inquirer from "inquirer";
+
+interface ansType {
+    userId:string,
+    userPin:number,
+    accountType:
+}
+
+const answers: ansType = await inquirer.prompt([
+{
+type: "input",
+name: "userId",
+message: "Kindly enter your ID"
+},
+{
+ type: "number",
+ name: "userPin", 
+ message : "Kindly enter you PIN"   
+},
+{
+    type:"list",
+    name:"accountType",
+    choices: ["current", "savings"],
+    message: "Select your account",
+  
+}, 
+{
+    type:"list",
+    name:"transactionType",
+    choices: ["Fast Cash", "Withdraw"],
+    message: "Select your transaction",
+    when(answers){
+        return answers.accountType
+    },
+},
+{
+    type:"list",
+    name:"amount",
+    choices: [1000, 2000, 10000, 20000],
+    message: "Select your amount",
+    when(answers){
+        return answers.transactionType == "Fast Cash"
+    }
+    },
+    {
+        type:"number",
+        name:"amount",
+        message: "Enter your amount",
+        when(answers){
+            return answers.transactionType == "Withdraw"
+        },
+    }
+
+])
+
